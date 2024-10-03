9580a85fbff17647e71b42337fe94a70d0481361
mynolog <minholee.log@gmail.com>
2024-10-04 02:54:01 +0900

    style: 나의 페이지 UI 개선

--- src/components/partials/footer/Footer.tsx
+++ src/components/partials/footer/Footer.tsx
@@ -3,7 +3,7 @@ import { FaInstagram } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <footer className="w-3/4 mx-auto my-12  flex flex-col justify-center items-center gap-6 text-sm text-gray-500">
+    <footer className="min-h-fit mt-24 mb-12 flex flex-col justify-center items-center text-sm text-gray-500">
       <div className="flex items-center gap-4 mb-12">
         <span>© 2024 mynolog All rights reserved.</span>
         <a href="https://github.com/mynolog/mynogram" target="_parent ">
--- src/components/partials/main/UserOverview.tsx
+++ src/components/partials/main/UserOverview.tsx
@@ -2,7 +2,7 @@ import UserPost from './UserPost.tsx'
 import UserProfile from './UserProfile.tsx'
 const UserOverview = () => {
   return (
-    <main className="max-w-fit flex flex-col mx-auto mt-20">
+    <main className="max-w-fit min-w-[800px] flex flex-col mx-auto mt-20">
       <UserProfile />
       <UserPost />
     </main>
