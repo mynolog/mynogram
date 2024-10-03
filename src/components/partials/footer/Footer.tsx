b138c8e109d11dd54e8782a9775e6fb607ae2a48
mynolog <minholee.log@gmail.com>
2024-10-03 23:16:30 +0900

    style: 푸터 내용 및 스타일 변경

--- src/components/partials/footer/Footer.tsx
+++ src/components/partials/footer/Footer.tsx
@@ -1,27 +1,17 @@
-import { useState } from 'react'
-import footerData from './footer.json'
-import { Link } from 'react-router-dom'
+import { VscGithubInverted } from 'react-icons/vsc'
+import { FaInstagram } from 'react-icons/fa'
+
 const Footer = () => {
-  const [footerItems, _] = useState(Object.freeze(footerData.footer))
   return (
-    <footer className="w-3/4 m-auto  flex flex-col justify-center items-center gap-6 text-sm text-gray-500">
-      <ul className="w-full flex justify-center items-center gap-3">
-        {footerItems.length > 0 &&
-          footerItems.map((item) => (
-            <li className="hover:underline" key={item.id + item.title}>
-              <Link to={item.to}>{item.title}</Link>
-            </li>
-          ))}
-      </ul>
-      <div className="flex gap-4">
-        <select>
-          <option>한국어</option>
-          <option>日本語</option>
-          <option>English</option>
-          <option>Español</option>
-          <option>Français</option>
-        </select>
-        <span>© 2024 Mynogram from mynolog</span>
+    <footer className="w-3/4 mx-auto my-12  flex flex-col justify-center items-center gap-6 text-sm text-gray-500">
+      <div className="flex items-center gap-4 mb-12">
+        <span>© 2024 mynolog All rights reserved.</span>
+        <a href="https://github.com/mynolog/mynogram" target="_parent ">
+          <VscGithubInverted className="text-3xl" />
+        </a>
+        <a href="https://www.instagram.com/_undefined_2/" target="_parent ">
+          <FaInstagram className="text-3xl" />
+        </a>
       </div>
     </footer>
   )
