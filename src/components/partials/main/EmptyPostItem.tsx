f417ecfc926eed4622f8845d82a2ed1868aec837
mynolog <minholee.log@gmail.com>
2024-10-03 09:26:28 +0900

    fix: HTML 태그 규칙 위배 수정(li > li)

--- src/components/partials/main/EmptyPostItem.tsx
+++ src/components/partials/main/EmptyPostItem.tsx
@@ -1,11 +1,11 @@
 import { FaCameraRetro } from 'react-icons/fa'
 const EmptyPostItem = () => {
   return (
-    <li className="w-full flex flex-col items-center justify-center gap-4 mt-10">
+    <div className="w-full flex flex-col items-center justify-center gap-4 mt-10">
       <FaCameraRetro className="text-7xl" />
       <span className="text-2xl font-bold">게시물 없음</span>
       <p>왼쪽 탭에서 만들기를 눌러서 일상을 공유해보세요.</p>
-    </li>
+    </div>
   )
 }
 
